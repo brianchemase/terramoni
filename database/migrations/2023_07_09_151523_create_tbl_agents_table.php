41b451f8d312e8549bb93d4504c87c0e0cf81e9c
brianchemase <brianchemo@gmail.com>
2023-07-10 23:22:25 +0300

    upload update on migrations

--- database/migrations/2023_07_09_151523_create_tbl_agents_table.php
+++ database/migrations/2023_07_09_151523_create_tbl_agents_table.php
@@ -16,16 +16,17 @@ return new class extends Migration
         Schema::create('tbl_agents', function (Blueprint $table) {
             $table->increments('id');
             $table->string('first_name', 50);
+            $table->string('mid_name', 50);
             $table->string('last_name', 50);
             $table->string('phone', 50);
             $table->string('email', 50);
             $table->string('gender', 50);
-            $table->string('location', 5);
-            $table->string('country', 7);
-            $table->string('status', 9);
+            $table->string('location',225);
+            $table->string('country', 225);
+            $table->string('status', 225);
             $table->string('BVN', 50);
             $table->string('national_id_no', 50);
-            $table->string('passport', 7);
+            $table->string('passport', 225);
             $table->date('registration_date');
             $table->date('validation_date');
             $table->timestamps();
--- database/migrations/2023_07_10_171002_create_transactions_table.php
+++ database/migrations/2023_07_10_171002_create_transactions_table.php
@@ -14,7 +14,26 @@ return new class extends Migration
     public function up()
     {
         Schema::create('tbl_transactions', function (Blueprint $table) {
-            $table->id();
+            $table->string('Id', 10);
+            $table->string('Name', 255);
+            $table->string('BillerName', 255);
+            $table->string('ConsumerIdField', 255);
+            $table->string('Code', 10);
+            $table->string('BillerType', 10);
+            $table->string('ItemFee', 10);
+            $table->string('Amount', 10);
+            $table->string('BillerId', 10);
+            $table->string('BillerCategoryId', 10);
+            $table->string('CurrencyCode', 10);
+            $table->string('CurrencySymbol', 10);
+            $table->string('ItemCurrencySymbol', 10)->nullable();
+            $table->boolean('IsAmountFixed');
+            $table->integer('SortOrder');
+            $table->integer('PictureId');
+            $table->string('PaymentCode', 10);
+            $table->string('UssdShortCode', 255);
+            $table->integer('AmountType');
+            $table->string('PaydirectItemCode', 10);
             $table->timestamps();
         });
     }
