80fcd187a67d34a6d9dbf1350ad9373859eb04c8
brianchemase <brianchemo@gmail.com>
2023-07-15 20:01:24 +0300

    upload

--- app/Http/Controllers/AirtimeController.php
+++ app/Http/Controllers/AirtimeController.php
@@ -16,8 +16,8 @@ class AirtimeController extends Controller
         $denomination = $request->input('denomination');
 
         $url = "https:/clients.primeairtime.com//api/topup/exec/$phoneNumber";
-        $authorization = "Bearer " . env('PRIME_BEARER_TOKEN'); // Retrieve the bearer token from the .env file
-        //$authorization = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiI2NGFmZjZhOTkyNTE4YTFjNjViOGM3YTciLCJleHAiOjE2ODk2MDkwNDQ2NjF9.oXthjBundp0Zq-4MOCghUkZ9mEEg6EndfThQGdqjBBs";
+        //$authorization = "Bearer " . env('PRIME_BEARER_TOKEN'); // Retrieve the bearer token from the .env file
+        $authorization = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiI2NGFmZjZhOTkyNTE4YTFjNjViOGM3YTciLCJleHAiOjE2ODk2MDkwNDQ2NjF9.oXthjBundp0Zq-4MOCghUkZ9mEEg6EndfThQGdqjBBs";
         $data = [
             "product_id" => "MFIN-5-OR",
             "denomination" => $denomination,
