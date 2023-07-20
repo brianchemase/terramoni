5e7aaaf6c7c3c855a5cb4fbffa44a5e38dd4c883
brianchemase <brianchemo@gmail.com>
2023-07-20 15:35:27 +0300

    upload the system update to store customer ref

--- app/Http/Controllers/AirtimeController.php
+++ app/Http/Controllers/AirtimeController.php
@@ -84,13 +84,15 @@ class AirtimeController extends Controller
         $target = $responseData['target'];
         $topupAmount = $responseData['topup_amount'];
         $paidCurrency = $responseData['paid_currency'];
+        $customer_reference = $responseData['customer_reference'];
     
         // Store the data into the tbl_transactions table using the DB facade
         DB::table('tbl_transactions')->insert([
             'Name' => $productId,
             'BillerName' => $target,
             'ConsumerIdField' => $agent_names,
-            'ItemFee' => $topupAmount,
+            'customer_reference' => $customer_reference,
+            'ItemFee' => $topupAmount,//customer_reference
             'CurrencySymbol' => $paidCurrency,
             'BillerType' => 'Airtime Top up',
         ]);
