9caaa6e932570d58e6fc025eb57fb8fc469596bb
brianchemase <brianchemo@gmail.com>
2023-08-04 08:30:08 +0300

    updated the scripts

--- app/Http/Controllers/BillPaymentController.php
+++ app/Http/Controllers/BillPaymentController.php
@@ -25,6 +25,7 @@ class BillPaymentController extends Controller
             'prepaid' => 'required',
             'denomination' => 'required',
             'agent_id' => 'required',
+            'client_phone' => 'required',
         ]);
 
         $input = request()->all();
@@ -34,6 +35,7 @@ class BillPaymentController extends Controller
         $prepaid = $request->input('prepaid');
         $product_id = $request->input('product_id');
         $agent_id = $request->input('agent_id');
+        $$toNumber = $request->input('client_phone');
 
         //return $meter;
 
@@ -124,6 +126,20 @@ class BillPaymentController extends Controller
             'date' => $todayDate,
             'type' => 'Debit',
         ]);
+        // Extract required values
+        $target = $responseData['target'];
+        $pin_code = $responseData['pin_code'];
+        $units = '';
+        if (preg_match('/Units\s*:\s*([\d.]+)/i', $data['pin_option1'], $matches)) {
+            $units = $matches[1];
+        }
+        $topup_amount = $responseData['topup_amount'];
+        $date = substr($responseData['time'], 0, 10); // Extract date part from the ISO 8601 timestamp
+
+        // Create the message
+        $message = "mtr:{$target}, Token:{$pin_code}, Units:{$units}, TknAmt: {$topup_amount}, Date: {$date}";
+
+        $response = $this->sendSMS($toNumber, $message);
 
 
         // Return the API response in a well-structured manner
@@ -167,5 +183,61 @@ class BillPaymentController extends Controller
         // Output the response as JSON
         return response()->json($response);
 
+    }
+    private function sendSMS(string $toNumber, string $message)
+    {
+        // Replace these with your actual credentials
+        $apiKey = '4e3f3b621a7b0aabb13f1691729e83f0eff6ab05dbaa6173f46d9cc7f6d56dc5';
+        $username = "dennis.mwebia";
+        $authorization = base64_encode($username . ':' . $apiKey);
+
+        
+        // URL to the API endpoint
+        $url = 'https://api.africastalking.com/version1/messaging';
+
+        // Request data
+        $data = [
+            'username' => $username,
+            'to' => $toNumber,
+            'message' => $message,
+           //'from' => 'myShortCode', // Change this if needed
+        ];
+
+        
+        // Set the API key in the headers
+        $headers = [
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/x-www-form-urlencoded',
+            'apiKey' => $apiKey,
+        ];
+
+         // Initialize cURL session
+        // Create cURL resource
+            $ch = curl_init();
+
+            // Set cURL options
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+                'Accept: application/json',
+                'Content-Type: application/x-www-form-urlencoded',
+                'apiKey: ' . $apiKey
+            ));
+            curl_setopt($ch, CURLOPT_POST, 1);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+            // Execute the cURL request and store the response
+            $response = curl_exec($ch);
+
+            // Check for cURL errors
+            if (curl_errno($ch)) {
+                echo 'cURL error: ' . curl_error($ch);
+            }
+
+            // Close cURL resource
+            curl_close($ch);
+
+            // Output the response from the API
+            echo $response;
     }
 }
