38e7be21e561bd44cec249ff73b9480229f87ce1
brianchemase <brianchemo@gmail.com>
2023-08-09 13:57:30 +0300

    upload files

--- app/Http/Controllers/BillPaymentController.php
+++ app/Http/Controllers/BillPaymentController.php
@@ -152,9 +152,11 @@ class BillPaymentController extends Controller
     {
         // Replace these variables with your actual values
         $apiUrl = 'https:/clients.primeairtime.com/api/billpay/country/NG/electricity';
-        //$accessToken = 'YOUR_BEARER_TOKEN';
+        $accessToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiI2NGFmZjZhOTkyNTE4YTFjNjViOGM3YTciLCJleHAiOjE2OTE3NDQ0MDIzMjB9.s8aB4U1tqN0oJiyA8zsTGC-4-P4LC0-4eEsC4yVVYvA';
         $token = DB::table('tbl_prime_token')->select('token')->orderBy('id', 'desc')->value('token');
-        $authorization = "Bearer " .$token; // Retrieve the bearer token from the construct
+        $authorization = "Bearer " .$accessToken; // Retrieve the bearer token from the construct
+
+        return $token;
 
         // Initialize cURL session
         $ch = curl_init();
