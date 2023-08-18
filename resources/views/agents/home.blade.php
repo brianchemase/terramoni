01afbb9cf0f74c6c2d7e30444952d5442690a25f
brianchemase <brianchemo@gmail.com>
2023-08-18 14:53:19 +0300

    upload

--- resources/views/agents/home.blade.php
+++ resources/views/agents/home.blade.php
@@ -13,10 +13,10 @@
 					
 					<div class="col-md-12">
 						<div class="row ">
-							<div class="col-xl-4 col-lg-6" data-tilt>
-								<div class="card l-bg-cherry">
+							<div class="col-xl-6 col-lg-6" data-tilt>
+								<div class="card l-bg-tera-dark">
 									<div class="card-statistic-3 p-4">
-										<div class="card-icon card-icon-large"><i class="fas fa-money-bill-alt"></i></div>
+										<div class="card-icon card-icon-large"><i class="fas fa-concierge-bell"></i></div>
 										<div class="mb-4">
 											<h5 class="card-title mb-0" style="color: white;">Aggregated Wallet</h5>
 										</div>
@@ -35,7 +35,7 @@
 									</div>
 								</div>
 							</div>
-							<div class="col-xl-4 col-lg-6" data-tilt>
+							<div class="col-xl-6 col-lg-6" data-tilt>
 								<div class="card l-bg-cherry">
 									<div class="card-statistic-3 p-4">
 										<div class="card-icon card-icon-large"><i class="fas fa-money-bill-alt"></i></div>
@@ -136,7 +136,7 @@
 						</div>
 					</div>
 
-					<div class="row">
+					<!-- <div class="row">
 						<div class="col-sm-6 col-xl-3">
 							<div class="card">
 								<div class="card-body">
@@ -259,8 +259,186 @@
 								</div>
 							</div>
 						</div>
+					</div> -->
+					<h1 class="h3 mb-3"><strong>Top Performing Agents </strong> </h1>
+					<div class="row">
+						<div class="col-xl-12">
+							<div class="card">
+								<div class="card-header pb-0">
+									<div class="card-actions float-right">
+										<div class="dropdown show">
+											<a href="#" data-toggle="dropdown" data-display="static">
+												<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-more-horizontal align-middle"><circle cx="12" cy="12" r="1"></circle><circle cx="19" cy="12" r="1"></circle><circle cx="5" cy="12" r="1"></circle></svg>
+											</a>
+
+											<div class="dropdown-menu dropdown-menu-right">
+												<a class="dropdown-item" href="#">Action</a>
+												<a class="dropdown-item" href="#">Another action</a>
+												<a class="dropdown-item" href="#">Something else here</a>
+											</div>
+										</div>
+									</div>
+									<h5 class="card-title mb-0">Top Performing Agents</h5>
+								</div>
+								<div class="card-body">
+									<table class="table table-striped" style="width:100%">
+										<thead>
+											<tr>
+												<th>#</th>
+												<th>Name</th>
+												<th>Business Name</th>
+												<th>Email</th>
+												<th>Location</th>
+												<th>Earning</th>
+
+												<th>Status</th>
+											</tr>
+										</thead>
+										<tbody>
+											<tr>
+												<td><img src="dash/img/avatars/avatar.jpg" width="32" height="32" class="rounded-circle my-n1" alt="Avatar"></td>
+												<td>Garrett Winters</td>
+												<td>Good Guys</td>
+												<td>garrett@winters.com</td>
+												<td>Abuja</td>
+												<td>NRN 15,000</td>
+												<td><span class="badge bg-success">Active</span></td>
+											</tr>
+											<tr>
+												<td><img src="dash/img/avatars/avatar.jpg" width="32" height="32" class="rounded-circle my-n1" alt="Avatar"></td>
+												<td>Ashton Cox</td>
+												<td>Levitz Furniture</td>
+												<td>ashton@cox.com</td>
+												<td>Abuja</td>
+												<td>NRN 15,000</td>
+												<td><span class="badge bg-success">Active</span></td>
+											</tr>
+											<tr>
+												<td><img src="dash/img/avatars/avatar.jpg" width="32" height="32" class="rounded-circle my-n1" alt="Avatar"></td>
+												<td>Sonya Frost</td>
+												<td>Child World</td>
+												<td>sonya@frost.com</td>
+												<td>Abuja</td>
+												<td>NRN 15,000</td>
+												<td><span class="badge bg-danger">Deleted</span></td>
+											</tr>
+											<tr>
+												<td><img src="dash/img/avatars/avatar.jpg" width="32" height="32" class="rounded-circle my-n1" alt="Avatar"></td>
+												<td>Jena Gaines</td>
+												<td>Helping Hand</td>
+												<td>jena@gaines.com</td>
+												<td>Abuja</td>
+												<td>NRN 15,000</td>
+												<td><span class="badge bg-warning">Inactive</span></td>
+											</tr>
+											<tr>
+												<td><img src="dash/img/avatars/avatar-2.jpg" width="32" height="32" class="rounded-circle my-n1" alt="Avatar"></td>
+												<td>Charde Marshall</td>
+												<td>Price Savers</td>
+												<td>charde@marshall.com</td>
+												<td>Abuja</td>
+												<td>NRN 15,000</td>
+												<td><span class="badge bg-success">Active</span></td>
+											</tr>
+											<tr>
+												<td><img src="dash/img/avatars/avatar-2.jpg" width="32" height="32" class="rounded-circle my-n1" alt="Avatar"></td>
+												<td>Haley Kennedy</td>
+												<td>Helping Hand</td>
+												<td>haley@kennedy.com</td>
+												<td>Abuja</td>
+												<td>NRN 15,000</td>
+												<td><span class="badge bg-danger">Deleted</span></td>
+											</tr>
+											<tr>
+												<td><img src="dash/img/avatars/avatar-2.jpg" width="32" height="32" class="rounded-circle my-n1" alt="Avatar"></td>
+												<td>Tatyana Fitzpatrick</td>
+												<td>Good Guys</td>
+												<td>tatyana@fitzpatrick.com</td>
+												<td>Abuja</td>
+												<td>NRN 15,000</td>
+												<td><span class="badge bg-success">Active</span></td>
+											</tr>
+											<tr>
+												<td><img src="dash/img/avatars/avatar-3.jpg" width="32" height="32" class="rounded-circle my-n1" alt="Avatar"></td>
+												<td>Michael Silva</td>
+												<td>Red Robin Stores</td>
+												<td>michael@silva.com</td>
+												<td>Abuja</td>
+												<td>NRN 15,000</td>
+												<td><span class="badge bg-success">Active</span></td>
+											</tr>
+											<tr>
+												<td><img src="dash/img/avatars/avatar-3.jpg" width="32" height="32" class="rounded-circle my-n1" alt="Avatar"></td>
+												<td>Angelica Ramos</td>
+												<td>The Wiz</td>
+												<td>angelica@ramos.com</td>
+												<td>Abuja</td>
+												<td>NRN 15,000</td>
+												<td><span class="badge bg-success">Active</span></td>
+											</tr>
+											<tr>
+												<td><img src="dash/img/avatars/avatar-4.jpg" width="32" height="32" class="rounded-circle my-n1" alt="Avatar"></td>
+												<td>Jennifer Chang</td>
+												<td>Helping Hand</td>
+												<td>jennifer@chang.com</td>
+												<td>Abuja</td>
+												<td>NRN 15,000</td>
+												<td><span class="badge bg-warning">Inactive</span></td>
+											</tr>
+											<tr>
+												<td><img src="dash/img/avatars/avatar-4.jpg" width="32" height="32" class="rounded-circle my-n1" alt="Avatar"></td>
+												<td>Brenden Wagner</td>
+												<td>The Wiz</td>
+												<td>brenden@wagner.com</td>
+												<td>Abuja</td>
+												<td>NRN 15,000</td>
+												<td><span class="badge bg-warning">Inactive</span></td>
+											</tr>
+											<tr>
+												<td><img src="dash/img/avatars/avatar-4.jpg" width="32" height="32" class="rounded-circle my-n1" alt="Avatar"></td>
+												<td>Fiona Green</td>
+												<td>The Sample</td>
+												<td>fiona@green.com</td>
+												<td>Abuja</td>
+												<td>NRN 15,000</td>
+												<td><span class="badge bg-warning">Inactive</span></td>
+											</tr>
+											<tr>
+												<td><img src="dash/img/avatars/avatar-5.jpg" width="32" height="32" class="rounded-circle my-n1" alt="Avatar"></td>
+												<td>Prescott Bartlett</td>
+												<td>The Sample</td>
+												<td>prescott@bartlett.com</td>
+												<td>Abuja</td>
+												<td>NRN 15,000</td>
+												<td><span class="badge bg-success">Active</span></td>
+											</tr>
+											<tr>
+												<td><img src="dash/img/avatars/avatar-5.jpg" width="32" height="32" class="rounded-circle my-n1" alt="Avatar"></td>
+												<td>Gavin Cortez</td>
+												<td>Red Robin Stores</td>
+												<td>gavin@cortez.com</td>
+												<td>Abuja</td>
+												<td>NRN 15,000</td>
+												<td><span class="badge bg-success">Active</span></td>
+											</tr>
+											<tr>
+												<td><img src="dash/img/avatars/avatar-5.jpg" width="32" height="32" class="rounded-circle my-n1" alt="Avatar"></td>
+												<td>Howard Hatfield</td>
+												<td>Price Savers</td>
+												<td>howard@hatfield.com</td>
+												<td>Abuja</td>
+												<td>NRN 15,000</td>
+												<td><span class="badge bg-warning">Inactive</span></td>
+											</tr>
+										</tbody>
+									</table>
+								</div>
+							</div>
+						</div>
+
+						
 					</div>
-					
+
 			
 
 
