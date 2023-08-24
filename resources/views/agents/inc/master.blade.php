94777a1c19b3834f4b8cb98db064c40907b41676
brianchemase <brianchemo@gmail.com>
2023-08-24 11:25:44 +0300

    upload files

--- resources/views/agents/inc/master.blade.php
+++ resources/views/agents/inc/master.blade.php
@@ -103,6 +103,20 @@
 		});
 	</script>
 
+	<!--Start of Tawk.to Script-->
+		<script type="text/javascript">
+		var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+		(function(){
+		var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+		s1.async=true;
+		s1.src='https://embed.tawk.to/64e712fe94cf5d49dc6c38e7/1h8ja4d62';
+		s1.charset='UTF-8';
+		s1.setAttribute('crossorigin','*');
+		s0.parentNode.insertBefore(s1,s0);
+		})();
+		</script>
+		<!--End of Tawk.to Script-->
+
 	
 	
 
--- resources/views/agents/manageusers.blade.php
+++ resources/views/agents/manageusers.blade.php
@@ -13,7 +13,7 @@
 						<div class="col-12">
 							<div class="card">
 								<div class="card-header">
-									<h5 class="card-title mb-0">Empty card</h5>
+									<h5 class="card-title mb-0">Register TerraMoni User</h5>
 								</div>
 								<div class="card-body">
 								</div>
