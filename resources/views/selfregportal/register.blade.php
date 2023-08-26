9742e6b06235dceada6bf6dcf2eff297e072f7cd
brianchemase <brianchemo@gmail.com>
2023-08-26 20:12:43 +0300

    upload the solepropriotor form

--- resources/views/selfregportal/register.blade.php
+++ resources/views/selfregportal/register.blade.php
@@ -262,6 +262,84 @@
             <input type="text" name="baddress" id="baddress" required/>
         </div>
     </div>
+    <hr>
+    <h3>For Sole Proprietor Registration  </h3>
+
+    <div class="form-row">
+        <div class="form-group">
+            <label for="taxid"> Business Certificate: </label>
+            <input type="file" name="bizCert" id="bizCert">
+        </div>
+        <div class="form-group">
+            <label for="email">Business Registration Number :</label>
+            <input type="text" name="baddress" id="baddress" required/>
+        </div>
+    </div>
+
+    <div class="form-row">
+        <div class="form-group">
+            <label for="taxid"> Business Address: </label>
+            <input type="text" name="bizaddress" id="bizaddress">
+        </div>
+       
+        <div class="form-group">
+            <label for="state">State :</label>
+            <div class="form-select">
+                <select onchange="toggleLGA(this);" name="bizstate" id="state" class="form-control">
+                   
+                <option value="" selected="selected" disabled>- Select -</option>
+							<option value="Abia">Abia</option>
+							<option value="Adamawa">Adamawa</option>
+							<option value="AkwaIbom">AkwaIbom</option>
+							<option value="Anambra">Anambra</option>
+							<option value="Bauchi">Bauchi</option>
+							<option value="Bayelsa">Bayelsa</option>
+							<option value="Benue">Benue</option>
+							<option value="Borno">Borno</option>
+							<option value="Cross River">Cross River</option>
+							<option value="Delta">Delta</option>
+							<option value="Ebonyi">Ebonyi</option>
+							<option value="Edo">Edo</option>
+							<option value="Ekiti">Ekiti</option>
+							<option value="Enugu">Enugu</option>
+							<option value="FCT">FCT</option>
+							<option value="Gombe">Gombe</option>
+							<option value="Imo">Imo</option>
+							<option value="Jigawa">Jigawa</option>
+							<option value="Kaduna">Kaduna</option>
+							<option value="Kano">Kano</option>
+							<option value="Katsina">Katsina</option>
+							<option value="Kebbi">Kebbi</option>
+							<option value="Kogi">Kogi</option>
+							<option value="Kwara">Kwara</option>
+							<option value="Lagos">Lagos</option>
+							<option value="Nasarawa">Nasarawa</option>
+							<option value="Niger">Niger</option>
+							<option value="Ogun">Ogun</option>
+							<option value="Ondo">Ondo</option>
+							<option value="Osun">Osun</option>
+							<option value="Oyo">Oyo</option>
+							<option value="Plateau">Plateau</option>
+							<option value="Rivers">Rivers</option>
+							<option value="Sokoto">Sokoto</option>
+							<option value="Taraba">Taraba</option>
+							<option value="Yobe">Yobe</option>
+							<option value="Zamfara">Zamafara</option>
+                
+                </select>
+                <span class="select-icon"><i class="zmdi zmdi-chevron-down"></i></span>
+            </div>
+        </div>
+        <div class="form-group">
+            <label for="city">LGA :</label>
+            <div class="form-select">
+                <select name="bizlga" id="lga" class="form-control select-lga" required>
+                    
+                </select>
+                <span class="select-icon"><i class="zmdi zmdi-chevron-down"></i></span>
+            </div>
+        </div>
+    </div>
 
     <div class="form-submit">
         <input type="reset" value="Reset All" class="submit" name="reset" id="reset" />
