5a3179472a2566b5d5ccec9c9258eacab3647409
KEN21705-(Kevin Madiavale) <kmlivoi@kcbgroup.com>
2023-09-01 11:38:25 +0300

    fix: added state dropdowns

--- resources/views/agents/modals/create.blade.php
+++ resources/views/agents/modals/create.blade.php
@@ -32,12 +32,54 @@
                 <!-- ROW 1 -->
                 <div class="row g-3">
                     <div class="col-md-4">
+                        
                         <label for="state_id" class="form-label">State</label>
-                        <input type="number" class="form-control" id="state_id" name="state_id" placeholder="Enter State ID">
+                        <select onchange="toggleLGA(this);" name="state" id="state" class="form-control" name="state_id">
+							<option value="" selected="selected">- Select -</option>
+							<option value="Abia">Abia</option>
+							<option value="Adamawa">Adamawa</option>
+							<option value="AkwaIbom">AkwaIbom</option>
+							<option value="Anambra">Anambra</option>
+							<option value="Bauchi">Bauchi</option>
+							<option value="Bayelsa">Bayelsa</option>
+							<option value="Benue">Benue</option>
+							<option value="Borno">Borno</option>
+							<option value="Cross River">Cross River</option>
+							<option value="Delta">Delta</option>
+							<option value="Ebonyi">Ebonyi</option>
+							<option value="Edo">Edo</option>
+							<option value="Ekiti">Ekiti</option>
+							<option value="Enugu">Enugu</option>
+							<option value="FCT">FCT</option>
+							<option value="Gombe">Gombe</option>
+							<option value="Imo">Imo</option>
+							<option value="Jigawa">Jigawa</option>
+							<option value="Kaduna">Kaduna</option>
+							<option value="Kano">Kano</option>
+							<option value="Katsina">Katsina</option>
+							<option value="Kebbi">Kebbi</option>
+							<option value="Kogi">Kogi</option>
+							<option value="Kwara">Kwara</option>
+							<option value="Lagos">Lagos</option>
+							<option value="Nasarawa">Nasarawa</option>
+							<option value="Niger">Niger</option>
+							<option value="Ogun">Ogun</option>
+							<option value="Ondo">Ondo</option>
+							<option value="Osun">Osun</option>
+							<option value="Oyo">Oyo</option>
+							<option value="Plateau">Plateau</option>
+							<option value="Rivers">Rivers</option>
+							<option value="Sokoto">Sokoto</option>
+							<option value="Taraba">Taraba</option>
+							<option value="Yobe">Yobe</option>
+							<option value="Zamfara">Zamafara</option>
+						</select>
+                        <!-- <input type="number" class="form-control" id="state" name="state_id" placeholder="Enter State ID"> -->
                     </div>
                     <div class="col-md-4">
                         <label for="lga_id" class="form-label">LGA</label>
-                        <input type="number" class="form-control" id="lga_id" name="lga_id" placeholder="Enter LGA ID">
+                        <select name="lga" id="lga" class="form-control select-lga" name="lga_id" required></select>
+                        <!-- <input type="number" class="form-control" id="lga" name="lga_id" placeholder="Enter LGA ID"> -->
                     </div>
                     <div class="col-md-4">
                         <label for="biller_id" class="form-label">Biller</label>
